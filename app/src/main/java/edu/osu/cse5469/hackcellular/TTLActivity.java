04da180fa9c913495aaf1e956d438b255cd186c1
Yuhui Feng <fengyuhui@FelixdeMacBook-Pro.local>
2015-10-09 22:46:02 -0400

    socket function added

--- app/src/main/java/edu/osu/cse5469/hackcellular/TTLActivity.java
+++ app/src/main/java/edu/osu/cse5469/hackcellular/TTLActivity.java
@@ -1,6 +1,5 @@
 package edu.osu.cse5469.hackcellular;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,43 +7,94 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import java.net.Socket;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class TTLActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button bt;
+    private Button sendSocket;
     private EditText desIP;
     private EditText desPort;
     private EditText ttlTime;
-    private String ipAdr;
+    private TextView textHint;
+    private String ipAddr;
     private int portNum;
     private int ttl;
+    private String result = "";
 
-    private Socket client =null;
+
+    private DatagramSocket client;
+    private final int listenPort = 5501;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ttl);
 
-        bt = (Button) findViewById(R.id.sendButton);
+        sendSocket = (Button) findViewById(R.id.sendButton);
         desIP = (EditText) findViewById(R.id.edited_ip);
         desPort = (EditText) findViewById(R.id.edited_port);
         ttlTime = (EditText) findViewById(R.id.edited_ttl);
+        textHint = (TextView) findViewById(R.id.textHint);
 
-        ipAdr = desIP.getText().toString();
+        ipAddr = desIP.getText().toString();
         String tmp = desPort.getText().toString();
         portNum = Integer.parseInt(tmp);
         tmp = ttlTime.getText().toString();
         ttl = Integer.parseInt(tmp);
 
-        bt.setOnClickListener(this);
+        sendSocket.setOnClickListener(this);
+
+        // Listen to the response msg
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    DatagramSocket listener = new DatagramSocket(listenPort);
+                    byte[] inData = new byte[1024];
+                    DatagramPacket inPacket = new DatagramPacket(inData, inData.length);
+                    listener.receive(inPacket);
+                    result = new String(inPacket.getData(), inPacket.getOffset(), inPacket.getLength());
+                    if(result.length()>0){
+                        textHint.setText("Please reduce your TTL");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
     }
 
     @Override
     public void onClick(View v) {
+        try {
+            // Send UDP packet
+            client = new DatagramSocket();
+            InetAddress serverAddr = InetAddress.getByName(ipAddr);
+            byte[] sendData = intToByteArray(ttl);
+            byte[] receiveData = new byte[1024];
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddr, portNum);
+            client.send(sendPacket);
+            textHint.setText("Msg has been sent");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    // Convert int to byte
+    public static byte[] intToByteArray(int i) {
+        byte[] result = new byte[4];
+        result[0] = (byte)((i >> 24) & 0xFF);
+        result[1] = (byte)((i >> 16) & 0xFF);
+        result[2] = (byte)((i >> 8) & 0xFF);
+        result[3] = (byte)(i & 0xFF);
+        return result;
     }
 
     @Override
